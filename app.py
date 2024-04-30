3f231216094aa6c8343892ce5366eda640f3f476
lingering126 <renyi7318@gmail.com>
2024-04-30 09:57:39 +0800

    add user register route

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import render_template, request, redirect, url_for
+from werkzeug.security import generate_password_hash
+from app import app, db
+from app.models import User
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            return render_template('register.html', message='Username already exists.')
+        hashed_password = generate_password_hash(password)
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', message='')
