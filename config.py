df6ef2d3126ae6dfed10de8490087c1125d104d2
lingering126 <renyi7318@gmail.com>
2024-05-05 23:20:17 +0800

    change database name

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     SECRET_KEY = 'G5505-win'
 
     # Database URI
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'database.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Debug mode
