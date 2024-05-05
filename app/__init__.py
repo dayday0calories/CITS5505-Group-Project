8c92dc79d608f233c26d838ecc6e2e3d02801fa8
lingering126 <renyi7318@gmail.com>
2024-05-05 23:19:36 +0800

    add database migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 
@@ -7,6 +9,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
     db.init_app(app)
+    migrate = Migrate(app, db)
     return app
 
 
