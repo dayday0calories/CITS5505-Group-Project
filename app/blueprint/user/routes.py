fe48c1b6f1df5820c914bc4bf2882ebcf8a6e477
lingering126 <renyi7318@gmail.com>
2024-05-13 14:02:35 +0800

    enhance flash message display, add current password confirmation

--- app/blueprint/user/routes.py
+++ app/blueprint/user/routes.py
@@ -5,8 +5,7 @@ from app.models.models import Post, Reply, User, db
 import os
 from werkzeug.utils import secure_filename
 from .forms import UpdatePictureForm, ChangePasswordForm
-from werkzeug.security import generate_password_hash
-
+from werkzeug.security import generate_password_hash,check_password_hash
 
 @user.route('/user/profile/<int:user_id>',methods=['GET', 'POST'])
 @login_required
@@ -32,14 +31,20 @@ def user_settings():
             picture_form.picture.data.save(filepath)
             current_user.profile_image_url = url_for('static', filename='uploads/' + filename)
             db.session.commit()
-            flash('Profile picture updated successfully.')
+            flash('Profile picture updated successfully.','success')
         return redirect(url_for('user.user_settings'))
 
     if password_form.validate_on_submit() and 'submit_password' in request.form:
-        current_user.password = generate_password_hash(password_form.password.data)
-        db.session.commit()
-        flash('Password updated successfully.')
-        return redirect(url_for('user.user_settings'))
+        if not check_password_hash(current_user.password_hash, password_form.current_password.data):
+            flash('Current password is wrong.', 'danger')
+        elif password_form.password.data != password_form.confirm_password.data:
+            flash('Passwords must match', 'danger')
+        else:
+            current_user.password_hash = generate_password_hash(password_form.password.data)
+            db.session.commit()
+            flash('Password updated successfully.','success')
+            return redirect(url_for('user.user_settings'))
+
 
     return render_template('user/user_settings.html', picture_form=picture_form, password_form=password_form, user=current_user)
 
