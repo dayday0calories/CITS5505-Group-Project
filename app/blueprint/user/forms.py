330b86f6adb880800a2b7c2471ce854a30ca9c95
lingering126 <renyi7318@gmail.com>
2024-05-13 14:01:46 +0800

    add a confirmation of current password when change password

--- app/blueprint/user/forms.py
+++ app/blueprint/user/forms.py
@@ -3,6 +3,8 @@ from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, EqualTo, ValidationError
 from werkzeug.security import check_password_hash
+from flask_login import current_user
+
 
 
 class UpdatePictureForm(FlaskForm):
@@ -10,6 +12,8 @@ class UpdatePictureForm(FlaskForm):
     submit_picture = SubmitField('Update Picture')
 
 class ChangePasswordForm(FlaskForm):
+    current_password = PasswordField('Current Password', validators=[DataRequired()])
     password = PasswordField('New Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm New Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
     submit_password = SubmitField('Change Password')
+
