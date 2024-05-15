9d16f360b074e03e856a2235489b8f16c6ed424b
MomobamiKiraly <zc504212731@gmail.com>
2024-05-15 17:59:01 +0800

    fixed bug

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from app import create_app,db
 from flask import Flask, request, render_template
-from app.models.models import Post  
-
+from app.models.models import Post
+from flask_login import current_user
 app = create_app()
 
 #just say hi
@@ -15,6 +15,9 @@ def search():
     query = request.args.get('q')
     search_type = request.args.get('search_type')
 
+    # Get current user if logged in
+    user = current_user if current_user.is_authenticated else None
+
     if query:
         if search_type == 'Titles':
             results = Post.query.filter(Post.title.ilike(f'%{query}%')).all()
@@ -27,7 +30,7 @@ def search():
     else:
         results = []
 
-    return render_template('search_results.html', results=results, query=query)
+    return render_template('search_results.html', results=results, query=query, user=user)  # Pass user to the template
 
 if __name__ == '__main__':
     with app.app_context():
